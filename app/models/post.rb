34606288f4a4f4cff2ed034d4148c6560b59443e
unsatiable <amadsen87@gmail.com>
2014-01-30 18:02:20 -0700

    adds ability to comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
+  has_many :comments
   scope :featured, -> { where(featured: true)}
   scope :published, -> { where("published_at <= ?", Time.now).order("published_at DESC") }
   attr_accessible :author, :body, :published_at, :title, :featured
+  validates :author, presence: true
 end
